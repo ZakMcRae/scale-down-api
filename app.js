6272f3716b00afd3774859ea8159728478e5f7fc
Zak McRae <zakmcrae@gmail.com>
2021-09-29 09:36:36 -0400

    setup auth middleware, add use, gen token endpoint
    setUserFromToken middleware for easier authorization

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
 
 // db config
 const mongoDb = process.env.DB_URL;
@@ -17,7 +18,32 @@ var userRouter = require("./routes/user");
 
 var app = express();
 
+// extracts userId and sets on req.userId if token sent in header
+//
+// can be used to check if authorized on routes
+// i.e. if !(req.userId) return res.status(401).send('Unauthorized')
+const setUserFromToken = async (req, res, next) => {
+  try {
+    const authHeader = req.headers.authorization;
+
+    if (authHeader === undefined) {
+      return next();
+    }
+
+    const tokenPayload = await jwt.verify(
+      authHeader.slice(7),
+      process.env.JWT_SECRET
+    );
+    req.userId = tokenPayload.user;
+
+    next();
+  } catch (err) {
+    return res.status(500).send("Something went wrong");
+  }
+};
+
 app.use(logger("dev"));
+app.use(setUserFromToken);
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
--- routes/user.js
+++ routes/user.js
@@ -1,7 +1,18 @@
 const User = require("../models/user");
 const router = require("express").Router();
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
+// get user info
+router.get("/", async (req, res, next) => {
+  if (req.userId) {
+    return res.send(req.userId);
+  }
+
+  res.send("Welcome Guest");
+});
+
+// create a new user
 router.post("/", async (req, res, next) => {
   // check if username exists in database and reject request if true
   const userInDb = await User.findOne({ userName: req.body.userName });
@@ -20,6 +31,31 @@ router.post("/", async (req, res, next) => {
   await newUser.save();
 
   res.send(`New User Created - ${newUser.userName}`);
+});
+
+// generate a 30day auth token for existing user
+router.post("/token", async (req, res, next) => {
+  try {
+    const user = await User.findOne({ userName: req.body.userName });
+
+    // check if user exists in database
+    if (user === null) {
+      return res.status(404).send("User not found");
+    }
+
+    // check if password is correct compared to stored hashedPassword
+    if (!(await bcrypt.compare(req.body.password, user.hashedPassword))) {
+      return res.status(401).send("Incorrect Password");
+    }
+
+    // create and return jwt containing user info
+    const token = await jwt.sign({ user: user.id }, process.env.JWT_SECRET, {
+      expiresIn: "30 days",
+    });
+    res.send({ token, token_type: "Bearer", expires_in: 2592000 });
+  } catch (err) {
+    return res.status(500).send("Something went wrong");
+  }
 });
 
 module.exports = router;
