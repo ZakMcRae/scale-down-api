15f24831cadd53951eca2af5f5aaeae066c89758
Zak McRae <zakmcrae@gmail.com>
2021-10-01 15:24:49 -0400

    reorganized test utilities to test/utils dir

---
+++ test/utils/test-db-setup.js
@@ -0,0 +1,36 @@
+const { beforeAll, beforeEach, afterAll } = require("@jest/globals");
+const { MongoMemoryServer } = require("mongodb-memory-server");
+const mongoose = require("mongoose");
+
+let mongoServer, mongoUri;
+
+// before all tests - setup and connect to new in memory database
+beforeAll(async () => {
+  mongoServer = await MongoMemoryServer.create();
+  mongoUri = mongoServer.getUri();
+  mongoose.connect(mongoUri);
+  mongoose.connection.on("error", (e) => {
+    if (e.message.code === "ETIMEDOUT") {
+      console.log(e);
+      mongoose.connect(mongoUri);
+    }
+    console.log(e);
+  });
+  mongoose.connection.once("open", () => {
+    console.log(`MongoDB successfully connected to ${mongoUri}`);
+  });
+});
+
+beforeEach(async () => {
+  // drop collections before each test - stops unwanted side effects between tests
+  const collections = Object.keys(mongoose.connection.collections);
+  collections.map(async (collection) => {
+    await mongoose.connection.collections[collection].deleteMany();
+  });
+});
+
+// after all tests - close and stop in memory database
+afterAll(async () => {
+  await mongoose.connection.close();
+  await mongoServer.stop();
+});
