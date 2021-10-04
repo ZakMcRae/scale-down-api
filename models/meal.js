cab8e2791ad1eefab826f14c87050e3c52d3b63d
Zak McRae <zakmcrae@gmail.com>
2021-10-04 16:17:01 -0400

    removed old useless comment

--- models/meal.js
+++ models/meal.js
@@ -5,7 +5,6 @@ var MealSchema = new mongoose.Schema(
     user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
     name: { type: String, required: true },
     date: { type: Date, default: Date.now },
-    //   foodItems: [{ type: mongoose.Schema.Types.ObjectId, ref: "FoodCount" }],
     foodList: [
       {
         foodItem: { type: mongoose.Schema.Types.ObjectId, ref: "FoodItem" },
